2d9e338149fad8676d818be0c570ebf2080e9f4e
AniketDubey <aniket.dubey0206@gmail.com>
2021-10-05 14:53:52 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Auth_Page(),
+      theme: ThemeData(
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Theme.of(context).primaryColor.withOpacity(0.9),
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Theme.of(context).primaryColor.withOpacity(0.3),
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              width: 2,
+              color: Theme.of(context).primaryColor.withOpacity(1),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
